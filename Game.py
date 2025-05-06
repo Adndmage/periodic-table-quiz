43356661a85d5b81d51d200e1352452aefa41d07
Adndmage <leoleyang@gmail.com>
2025-05-06 23:04:21 +0200

    Added full menu navigation until before starting the quiz

--- Game.py
+++ Game.py
@@ -8,8 +8,10 @@ class Game:
         self.clock = pg.time.Clock()
         self.running = True
 
+        # UI and navigation
         self.screen_color = "white" # Default screen color
         self.screen_number = 1 # 1 for main menu, 2 for question type, 3 for quiz type, -1 for settings
+        self.game_type = None
 
         # Quiz related attributes
         self.highscores = [] # Multidimensional list to store highscores for each quiz type
@@ -20,6 +22,10 @@ class Game:
     
     def set_screen(self, screen_number):
         self.screen_number = screen_number
+    
+    def set_screen_and_quiz_type(self, screen_number, game_type):
+        self.screen_number = screen_number
+        self.game_type = game_type
     
     def toggle_dark_mode(self):
         self.screen_color = "#323339" if self.screen_color == "white" else "white"
--- main.py
+++ main.py
@@ -31,6 +31,14 @@ game.running = True
 main_menu_text = pg.sprite.Group()
 main_menu_text.add(FontSprite(WIDTH/2, 120, "Periodic Table Quiz", "lucidasanstypewriter", 54))
 
+# Quiz type selection menu text
+game_selection_menu_text = pg.sprite.Group()
+game_selection_menu_text.add(FontSprite(WIDTH/2, 60, "Select Game Type", "lucidasanstypewriter", 54))
+
+# Question type selection menu text
+question_selection_menu_text = pg.sprite.Group()
+question_selection_menu_text.add(FontSprite(WIDTH/2, 60, "Select Question Type", "lucidasanstypewriter", 54))
+
 # Settings text
 settings_text = pg.sprite.Group()
 settings_text.add(FontSprite(WIDTH/2, 60, "Settings", "lucidasanstypewriter", 54))
@@ -53,14 +61,14 @@ def main():
                         game.screen_number = 1
                     elif game.screen_number == 3:
                         game.screen_number = 2
-        
+                    elif game.screen_number == 4:
+                        game.screen_number = 2
 
         # Draws the screen depending on the screen number
         game.screen.fill(game.screen_color)
 
         # Main menu screen
         if game.screen_number == 1:
-            game.screen.fill(game.screen_color)
             main_menu_text.draw(game.screen)
 
             button_3 = None
@@ -79,7 +87,7 @@ def main():
             inactiveColour="#FFFFFF",
             pressedColour="#C1E1C1",
             radius=2,
-            onClick=lambda: game.set_screen(2) # Set screen to question type
+            onClick=lambda: game.set_screen(2) # Set screen to game type
             )
 
             button_2 = Button(game.screen,
@@ -94,17 +102,121 @@ def main():
             inactiveColour="#FFFFFF",
             pressedColour="#C1E1C1",
             radius=2,
-            onClick=lambda: game.set_screen(-1) # Set screen to question type
+            onClick=lambda: game.set_screen(-1) # Set screen to settings
             )
 
-        elif game.screen_number == 2:            
-            button_1 = None
-            button_2 = None
-            button_3 = None
+        elif game.screen_number == 2:
+            game_selection_menu_text.draw(game.screen)
+
             button_4 = None
+
+            button_1 = Button(game.screen,
+            85, 235, 250, 100, # Coordinates and size
+            text="Multiple Choice",
+            font=pg.font.SysFont("lucidasanstypewriterregular", 20),
+            margin=10,
+            borderRadius=2,
+            borderThickness=2,
+            colour="#D3D3D3",
+            borderColour="#000000",
+            inactiveColour="#FFFFFF",
+            pressedColour="#C1E1C1",
+            radius=2,
+            onClick=lambda: game.set_screen_and_quiz_type(3, "multiple choice") # Set screen to question type and set game to Multiple Choice
+            )
+
+            button_2 = Button(game.screen,
+            385, 235, 250, 100, # Coordinates and size
+            text="Text Input",
+            font=pg.font.SysFont("lucidasanstypewriterregular", 20),
+            margin=10,
+            borderRadius=2,
+            borderThickness=2,
+            colour="#D3D3D3",
+            borderColour="#000000",
+            inactiveColour="#FFFFFF",
+            pressedColour="#C1E1C1",
+            radius=2,
+            onClick=lambda: game.set_screen_and_quiz_type(3, "text input") # Set screen to question type and set game to Text Input
+            )
+
+            button_3 = Button(game.screen,
+            235, 385, 250, 100, # Coordinates and size
+            text="True or False",
+            font=pg.font.SysFont("lucidasanstypewriterregular", 20),
+            margin=10,
+            borderRadius=2,
+            borderThickness=2,
+            colour="#D3D3D3",
+            borderColour="#000000",
+            inactiveColour="#FFFFFF",
+            pressedColour="#C1E1C1",
+            radius=2,
+            onClick=lambda: game.set_screen_and_quiz_type(3, "true or false") # Set screen to question type and set game to True or False
+            )
         
         elif game.screen_number == 3:
-            pass
+            question_selection_menu_text.draw(game.screen)
+
+            button_1 = Button(game.screen,
+            85, 235, 250, 100, # Coordinates and size
+            text="Atomic Number from Name",
+            font=pg.font.SysFont("lucidasanstypewriterregular", 16),
+            margin=10,
+            borderRadius=2,
+            borderThickness=2,
+            colour="#D3D3D3",
+            borderColour="#000000",
+            inactiveColour="#FFFFFF",
+            pressedColour="#C1E1C1",
+            radius=2,
+            onClick=lambda: game.set_screen_and_quiz_type(3, "multiple choice") # Set screen to question type and set game to Multiple Choice
+            )
+
+            button_2 = Button(game.screen,
+            385, 235, 250, 100, # Coordinates and size
+            text="Name from Atomic Number",
+            font=pg.font.SysFont("lucidasanstypewriterregular", 16),
+            margin=10,
+            borderRadius=2,
+            borderThickness=2,
+            colour="#D3D3D3",
+            borderColour="#000000",
+            inactiveColour="#FFFFFF",
+            pressedColour="#C1E1C1",
+            radius=2,
+            onClick=lambda: game.set_screen_and_quiz_type(3, "text input") # Set screen to question type and set game to Text Input
+            )
+
+            button_3 = Button(game.screen,
+            85, 385, 250, 100, # Coordinates and size
+            text="Symbol from Name",
+            font=pg.font.SysFont("lucidasanstypewriterregular", 16),
+            margin=10,
+            borderRadius=2,
+            borderThickness=2,
+            colour="#D3D3D3",
+            borderColour="#000000",
+            inactiveColour="#FFFFFF",
+            pressedColour="#C1E1C1",
+            radius=2,
+            onClick=lambda: game.set_screen_and_quiz_type(3, "true or false") # Set screen to question type and set game to True or False
+            )
+
+            button_4 = Button(game.screen,
+            385, 385, 250, 100, # Coordinates and size
+            text="Name from Symbol",
+            font=pg.font.SysFont("lucidasanstypewriterregular", 16),
+            margin=10,
+            borderRadius=2,
+            borderThickness=2,
+            colour="#D3D3D3",
+            borderColour="#000000",
+            inactiveColour="#FFFFFF",
+            pressedColour="#C1E1C1",
+            radius=2,
+            onClick=lambda: game.set_screen_and_quiz_type(3, "true or false") # Set screen to question type and set game to True or False
+            )
 
         elif game.screen_number == -1:
             settings_text.draw(game.screen)
@@ -124,13 +236,11 @@ def main():
             inactiveColour="#FFFFFF",
             pressedColour="#C1E1C1",
             radius=2,
-            onClick=lambda: game.toggle_dark_mode() # Set screen to question type
+            onClick=lambda: game.toggle_dark_mode() # Set screen to main menu
             )
-
         
         pygame_widgets.update(events)
         pg.display.flip()
-
         game.clock.tick(60)  # limits FPS to 60
 
 if __name__ == "__main__":
