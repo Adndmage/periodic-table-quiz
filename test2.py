5a3c838e0b4df2e62ce35e93b1889ccd175516fd
Adndmage <leoleyang@gmail.com>
2025-05-07 18:39:12 +0200

    Cleaned up unused files

--- test2.py
+++
@@ -1,8 +0,0 @@
-highscores = [[0, 0, 0, 0], # Multidimensional list to store highscores for each gamemode and question type
-            [0, 0, 0, 0], 
-            [0, 0, 0, 0]
-            ]
-
-highscores[1][3] = 22
-
-print(highscores)
